e628042cd34449c40127c1efda95cf342a22b75d
roshanpoudyal <roshanpoudyal.di@gmail.com>
2017-11-05 10:53:07 +0100

    Nodejs environment for tic tac toe app set up and the basic layout too

---
+++ ticTacToeServer.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressapp = express();
+
+expressapp.use(express.static('stylesheets'));
+expressapp.use(express.static('views'));
+
+expressapp.get('/index.html', function(req, res){
+    res.sendFile(__dirname + "/" + "index.html");
+});
+
+var expressServer = expressapp.listen(3000, function(){
+    var host = expressServer.address().address;
+    var port = expressServer.address().port;
+
+    console.log("Tic Tac Toe app listening at http://%s:%s", host, port);
+});
+  
